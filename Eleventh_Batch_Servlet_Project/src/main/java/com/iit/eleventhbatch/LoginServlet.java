02b000c77a8c2e8b9a9f6e4714c430db3ab6523d
Mehrin-Anannya <anannya.mehrin@gmail.com>
2022-08-30 16:36:18 +0600

    First Commit

---
+++ Eleventh_Batch_Servlet_Project/src/main/java/com/iit/eleventhbatch/LoginServlet.java
@@ -0,0 +1,56 @@
+package com.iit.eleventhbatch;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+//@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	//	doGet(request, response);
+		 String username = request.getParameter("username");
+		    // Send the response back to the user
+		    try { 
+		      response.setContentType("text/html");
+		      PrintWriter writer = response.getWriter();
+		      writer.println("<html><body>");
+		      writer.println("Thank you, " + username + ". You are now logged into the system.");
+		      writer.println("</body></html>");
+		      writer.close();
+		    } catch (Exception e) {
+		      e.printStackTrace();
+		    }
+
+	}
+
+}
